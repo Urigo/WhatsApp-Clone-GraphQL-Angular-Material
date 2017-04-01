0b862af0b9cca4476a70660a6aaf12065c4ba469
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-04-01 11:10:41 +0200

    Switch back to use adorable.io/avatars

--- src/app/shared/random-avatar.pipe.ts
+++ src/app/shared/random-avatar.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RandomAvatarPipe implements PipeTransform {
 
   transform(value: string): string {
-    return `http://avatar.3sd.me/40?name=${value}`;
-    // return `http://api.adorable.io/avatars/${value}`;
+    // return `http://avatar.3sd.me/40?name=${value}`;
+    return `http://api.adorable.io/avatars/${value}`;
   }
 
 }
